dd9fb964db72e2919079bfdcb3083d1e98d1e849
balbez <mainmail218@gmail.com>
2026-04-21 15:30:23 +0300

    4ую домашку завершил

--- script.js
+++ script.js
@@ -12,23 +12,51 @@ const servicePrice1 = +prompt("Сколько это будет стоить?");
 const service2 = prompt("Какой дополнительный тип услуги нужен?");
 const servicePrice2 = +prompt("Сколько это будет стоить?");
 
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
-if (fullPrice > 30_000) {
-    console.log("Даем скидку в 10%");
-} else if (fullPrice > 15_000 && fullPrice < 30_000) {
-    console.log("Даем скидку в 5%");
-} else if (fullPrice > 0 && fullPrice < 15_000) {
-    console.log("Скидка не предусмотрена");
-} else {
-    console.log("Что то пошло не так");
+const getAllServicePrices = function (serv1, serv2) {
+    return serv1 + serv2;
 }
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+function getFullPrice(screen, allServ) {
+    return screen + allServ;
+}
+
+const getTitle = function (title) {
+    const newTitle = title.trim().toLowerCase();
+    return newTitle[0].toUpperCase() + newTitle.substr(1);
+}
+
+const getServicePercentPrices = function (price, rollback) {
+    return Math.ceil(price - price * (rollback / 100));
+}
+
+const showTypeOf = function (obj) {
+    console.log(typeof obj);
+}
+
+const getRollbackMessage = function (price) {
+    if (price > 30_000) {
+        return "Даем скидку в 10%";
+    } else if (price > 15_000 && price < 30_000) {
+        return "Даем скидку в 5%";
+    } else if (price > 0 && price < 15_000) {
+        return "Скидка не предусмотрена";
+    } else {
+        return "Что то пошло не так";
+    }
+}
+
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+fullPrice = getFullPrice(screenPrice, allServicePrices);
+title = getTitle(title);
+const servicePercentPrice = getServicePercentPrices(fullPrice, rollback)
+console.log(getRollbackMessage(fullPrice));
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
 console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
 console.log(`Стоимость разработки сайта ${fullPrice} долларов`);
 console.log(screens.toLowerCase().split(', '));
-console.log(`Процент отката посреднику за работу: ${fullPrice * (rollback / 100)}`)
+console.log(`Процент отката посреднику за работу: ${servicePercentPrice}`)
